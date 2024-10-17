8548334ca5f133f8d46fcfd74f55a1b4a74044b6
Krayz7436 <krayz7436@gmail.com>
2024-10-17 13:55:30 -0500

    Update RemoteUpdate.bat

--- RemoteUpdate.bat
+++ RemoteUpdate.bat
@@ -1,3 +1,5 @@
+:updcheck
+color 2
 @echo off
 if /i not "%~1"=="updated" (
     echo Updating %~nx0 ...
@@ -8,7 +10,31 @@ if /i not "%~1"=="updated" (
 cd /d "%~dp0"
 pause
 @echo off
+cls
+color 2
+echo =================================================
+echo "               __                       __    ";
+echo "  ____   _____/  |___  _  _____________|  | __";
+echo " /    \_/ __ \   __\ \/ \/ /  _ \_  __ \  |/ /";
+echo "|   |  \  ___/|  |  \     (  <_> )  | \/    < ";
+echo "|___|  /\___  >__|   \/\_/ \____/|__|  |__|_ \";
+echo "     \/     \/                              \/";
+echo =================================================
+echo Checking Connection.
+ping -n 2 -w 700 www.google.com | find "bytes=" >nul
+IF %ERRORLEVEL% EQU 0 (
+    echo Connection Found.
+    goto Start
+) ELSE (
+    echo No Connection, running diagnostics.
+    TIMEOUT /T 1 /NOBREAK >nul
+    echo Please relaunch with a connection.
+    ipconfig /flushdns >nul
+    exit /b
+)
+:Start
 cls 
+color 4
 echo ============================
 echo " __                .__   ";
 echo " \ \    ____  __ __|  |  ";
@@ -17,12 +43,28 @@ echo "  / / |   |  \  |  /  |__";
 echo " /_/  |___|  /____/|____/";
 echo "           \/            ";
 echo ============================
+echo Check for updates[1]
+echo Console[2]
 SET /P Opt=Choice:
-if '%Opt%'=='1' goto :ex
-if '%Opt%'=='2' goto :ex
-:ex
-echo This is just an example.
-echo exiting
-TIMEOUT /T 3 /NOBREAK >nul
+if '%Opt%'=='1' goto :updcheck
+if '%Opt%'=='2' goto :Console
 exit /b
+:Console
+title Console
+color 4
+echo =================================================
+echo "                                 .__          ";
+echo "  ____  ____   ____   __________ |  |   ____  ";
+echo "_/ ___\/  _ \ /    \ /  ___/  _ \|  | _/ __ \ ";
+echo "\  \__(  <_> )   |  \\___ (  <_> )  |_\  ___/ "; 
+echo " \___  >____/|___|  /____  >____/|____/\___  >";
+echo "     \/           \/     \/                \/ ";
+echo =================================================
+echo Sucessfully Launched.
+:cmd
+set /p "cmd=%CD%>"
+%cmd%
+echo.
+goto cmd
+
 
