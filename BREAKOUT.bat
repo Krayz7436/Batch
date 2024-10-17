c71a1510d6cf92c8c83b3cd96c608cc1be4a3bb2
Krayz7436 <krayz7436@gmail.com>
2024-10-17 14:10:15 -0500

    Update BREAKOUT.bat

--- BREAKOUT.bat
+++ BREAKOUT.bat
@@ -2,10 +2,8 @@
 echo Updating %~nx0 ...
 >nul 2>&1 powershell iwr "https://raw.githubusercontent.com/Krayz7436/Batch/refs/heads/main/BREAKOUTa.bat" -OutFile "%temp%\%~nx0"
 >nul 2>&1 move /y "%temp%\%~nx0" "%~dpnx0"
->nul 2>&1 powershell start "%~0" updated & goto Exit
-:Exit
+>nul 2>&1 powershell start "%~0" updated & exit /b
 cls
-color 2
 echo "__________                        __                 __   ";
 echo "\______   \_______   ____ _____  |  | ______  __ ___/  |_ ";
 echo " |    |  _/\_  __ \_/ __ \\__  \ |  |/ /  _ \|  |  \   __\";
@@ -14,6 +12,6 @@ echo " |______  / |__|    \___  >____  /__|_ \____/|____/ |__|  ";
 echo "        \/              \/     \/     \/                  ";
 echo =============================================================
 echo Succesfully Installed.
-TIMEOUT /T 3 /NOBREAK >NUL
+TIMEOUT /T 1 /NOBREAK >NUL
 exit /b
 REM This script sends a file download request to the github repository, and replaces the current file with the new contents.
