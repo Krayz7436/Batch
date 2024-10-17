011e30e8056194a6279311a6bccd42f454c18544
Krayz7436 <krayz7436@gmail.com>
2024-10-17 14:19:40 -0500

    Update BREAKOUTa.bat

--- BREAKOUTa.bat
+++ BREAKOUTa.bat
@@ -6,6 +6,19 @@ echo "  \ \  \|\  \ \  \\  \\ \  \_|\ \ \  \ \  \ \  \\ \  \ \  \\\  \ \  \\\  \
 echo "   \ \_______\ \__\\ _\\ \_______\ \__\ \__\ \__\\ \__\ \_______\ \_______\   \ \__\";
 echo "    \|_______|\|__|\|__|\|_______|\|__|\|__|\|__| \|__|\|_______|\|_______|    \|__|";
 echo "                                                                                    ";
-echo ======================================================================================
+echo =======================================================================================
 echo Checking Connection.
+ping -n 2 -w 700 www.google.com | find "bytes=" >nul
+IF %ERRORLEVEL% EQU 0 (
+    echo Connection Found.
+    goto Start
+) ELSE (
+    echo No Connection, running diagnostics.
+    TIMEOUT /T 1 /NOBREAK >nul
+    echo Please relaunch with a connection.
+    ipconfig /flushdns >nul
+    exit /b
+)
+:Start
+cls
 
