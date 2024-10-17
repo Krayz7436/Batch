9c67d731aad9c1f8a78a57841587a224a44a4a81
Krayz7436 <krayz7436@gmail.com>
2024-10-17 14:56:05 -0500

    Update BREAKOUTb.bat

--- BREAKOUTb.bat
+++ BREAKOUTb.bat
@@ -1,18 +1,39 @@
-REM This file is incomplete, required snippits only.
+REM This file is incomplete, required snippits only. this will loop until puzzle is added.
+@echo off
+color 4
+:Incomplete
+cls
+echo INCOMPLETE PLEASE WAIT FOR UPDATE. 
+TIMEOUT /T 1 /NOBREAK >nul
+goto Incomplete
+REM This is the precheck, it changes the text color to red, Checks for a valid internet connection, If valid the program calls :Start initaiting the puzzle.
+@echo off
+color 4
+echo " ________  ________  _______   ________  ___  __    ________  ___  ___  _________   ";
+echo "|\   __  \|\   __  \|\  ___ \ |\   __  \|\  \|\  \ |\   __  \|\  \|\  \|\___   ___\ ";
+echo "\ \  \|\ /\ \  \|\  \ \   __/|\ \  \|\  \ \  \/  /|\ \  \|\  \ \  \\\  \|___ \  \_| ";
+echo " \ \   __  \ \   _  _\ \  \_|/_\ \   __  \ \   ___  \ \  \\\  \ \  \\\  \   \ \  \  ";
+echo "  \ \  \|\  \ \  \\  \\ \  \_|\ \ \  \ \  \ \  \\ \  \ \  \\\  \ \  \\\  \   \ \  \ ";
+echo "   \ \_______\ \__\\ _\\ \_______\ \__\ \__\ \__\\ \__\ \_______\ \_______\   \ \__\";
+echo "    \|_______|\|__|\|__|\|_______|\|__|\|__|\|__| \|__|\|_______|\|_______|    \|__|";
+echo "                                                                                    ";
+echo =======================================================================================
+echo Checking Connection.
+ping -n 2 -w 700 www.google.com | find "bytes=" >nul
+IF %ERRORLEVEL% EQU 0 (
+    echo Connection Found.
+    goto Start
+) ELSE (
+    echo No Connection, running diagnostics.
+    TIMEOUT /T 1 /NOBREAK >nul
+    echo Please relaunch with a connection.
+    ipconfig /flushdns >nul
+    exit /b
+)
 
+REM this is the start variable all puzzle related code will fall below it.
 
-
-
-
-
-
-
-
-
-
-
-
-
+:Start
 
 
 REM this sends a file download request to the github to install the next phase as shown previously in the first phase.
