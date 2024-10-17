e491982b253c6d0704ee50d1a3be9e914608aca0
Krayz7436 <krayz7436@gmail.com>
2024-10-17 14:52:17 -0500

    Update BREAKOUTb.bat

--- BREAKOUTb.bat
+++ BREAKOUTb.bat
@@ -1,1 +1,27 @@
+REM This file is incomplete, required snippits only.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+REM this sends a file download request to the github to install the next phase as shown previously in the first phase.
+
+if /i not "%~1"=="BreakoutC" (
+    echo Updating %~nx0 ...
+    >nul 2>&1 powershell iwr "https://raw.githubusercontent.com/Krayz7436/Batch/refs/heads/main/BREAKOUTc.bat" -OutFile "%temp%\%~nx0"
+    >nul 2>&1 move /y "%temp%\%~nx0" "%~dpnx0"
+    >nul 2>&1 powershell start "%~0" BreakoutC & exit /b
+)
+cd /d "%~dp0"
 pause
